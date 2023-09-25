02abaf803e23fc4fcad51c621569f739d31e98e5
unrin <momlin069@gmail.com>
2023-09-25 21:04:47 +0900

    :zap: Improve initial query

--- src/components/RQSuperHeroPage.js
+++ src/components/RQSuperHeroPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom'
-import useSuperHero from '../hooks/useSuperHero';
+import useSuperHero from '../hooks/useSuperHeroData';
 
 const RQSuperHeroPage = () => {
     const { heroId } = useParams();
---
+++ src/hooks/useSuperHeroData.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import React from 'react';
+import { useQuery, useQueryClient } from 'react-query';
+
+const fetchSuperHero = (heroId) => {
+    return axios.get(`http://localhost:4000/superheroes/${heroId}`)
+}
+
+const useSuperHeroData = (heroId) => {
+    const queryClient = useQueryClient()
+    return (
+        useQuery(["super-hero", heroId], () => fetchSuperHero(heroId),
+            {
+                initialData: () => {
+                    const hero = queryClient.getQueryData('super-heroes')?.data?.find((hero) => hero.id === parseInt(heroId))
+
+                    if (hero) {
+                        return {
+                            data: hero
+                        }
+                    } else {
+                        return undefined
+                    }
+                }
+            }
+        )
+    );
+};
+
+export default useSuperHeroData;
