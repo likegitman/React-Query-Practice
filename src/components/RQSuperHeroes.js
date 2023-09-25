835984fa96f6d018949ab3e0b9abd724c30295a8
unrin <momlin069@gmail.com>
2023-09-25 23:24:48 +0900

    :sparkles: Add useMutation features

--- src/components/RQSuperHeroes.js
+++ src/components/RQSuperHeroes.js
@@ -1,7 +1,12 @@
-import useSuperHeroesData from '../hooks/useSuperHeroesData';
+import useSuperHeroesData, { useAddSuperHeroData } from '../hooks/useSuperHeroesData';
 import { Link } from 'react-router-dom'
+import { useState } from 'react';
 
 const RQSuperHeroes = () => {
+
+    const [name, setName] = useState('')
+    const [alterEgo, setAlterEgo] = useState('')
+
     const onSuccess = (data) => {
         console.log(data);
     }
@@ -12,14 +17,34 @@ const RQSuperHeroes = () => {
 
     const { isLoading, data, isError, error, isFetching, refetch } = useSuperHeroesData(onSuccess, onError)
 
-    console.log({ isLoading, isFetching })
+    const { mutate: addHero } = useAddSuperHeroData()
+
+    const handleAddHeroClicked = () => {
+        console.log({ name, alterEgo })
+        const hero = { name, alterEgo }
+        addHero(hero)
+    }
 
     if (isLoading || isFetching) return <h2>Loading...</h2>
 
     if (isError) return <h2>{error.message}</h2>
+
     return (
         <div>
             <h2>RQ Super Heroes Page</h2>
+            <div>
+                <input
+                    type='text'
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <input
+                    type='text'
+                    value={alterEgo}
+                    onChange={(e) => setAlterEgo(e.target.value)}
+                />
+                <button onClick={handleAddHeroClicked}>Add Hero</button>
+            </div>
             <button onClick={refetch}>Fetch heroes</button>
             {data?.data.map((hero) => {
                 return <div key={hero.id}>
--- src/hooks/useSuperHeroesData.js
+++ src/hooks/useSuperHeroesData.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
-import { useQuery } from 'react-query';
+import { useQuery, useMutation } from 'react-query';
 
 const fetchSuperHeroes = () => {
     return axios.get('http://localhost:4000/superheroes')
+}
+
+const addSuperHero = (hero) => {
+    return axios.post('http://localhost:4000/superheroes', hero)
 }
 
 const useSuperHeroesData = (onSuccess, onError) => {
@@ -26,5 +30,9 @@ const useSuperHeroesData = (onSuccess, onError) => {
         )
     );
 };
+
+export const useAddSuperHeroData = () => {
+    return useMutation(addSuperHero)
+}
 
 export default useSuperHeroesData;
