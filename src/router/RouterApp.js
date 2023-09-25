cbd847f40e1d783c479c4847304968ec765e854b
unrin <momlin069@gmail.com>
2023-09-25 18:15:36 +0900

    :zap: Improve parallel query

--- src/router/RouterApp.js
+++ src/router/RouterApp.js
@@ -4,6 +4,8 @@ import HomePage from '../components/Hom';
 import RQSuperHeroPage from '../components/RQSuperHeroPage';
 import RQSuperHeroes from '../components/RQSuperHeroes';
 import SuperHeroes from '../components/SuperHeroes';
+import ParallelQueriesPage from '../components/ParallelQueriesPage';
+import DynamicParallelPage from '../components/DynamicParallelPage';
 
 const RouterApp = () => {
     return (
@@ -12,6 +14,8 @@ const RouterApp = () => {
             <Route path='/super-heroes' element={<SuperHeroes />} />
             <Route path='/rq-super-heroes' element={<RQSuperHeroes />} />
             <Route path='/rq-super-heroes/:heroId' element={<RQSuperHeroPage />} />
+            <Route path='/rq-parallel' element={<ParallelQueriesPage />} />
+            <Route path='/rq-dynamic-parallel' element={<DynamicParallelPage heroIds={[1, 3]} />} />
         </Routes>
     );
 };
