4eeaaed8c551b379ab1742e41223ac74dfaa65df
unrin <momlin069@gmail.com>
2023-09-25 14:59:21 +0900

    :sparkles: Add user event refetch

--- src/components/RQSuperHeroes.js
+++ src/components/RQSuperHeroes.js
@@ -6,7 +6,7 @@ const fetchSuperHeroes = () => {
     return axios.get('http://localhost:4000/superheroes')
 }
 const RQSuperHeroes = () => {
-    const { isLoading, data, isError, error, isFetching } = useQuery('super-heroes', fetchSuperHeroes,
+    const { isLoading, data, isError, error, isFetching, refetch } = useQuery('super-heroes', fetchSuperHeroes,
         {
             // cacheTime: 5000, // 5초 동안 데이터를 캐싱함
             // staleTime: 30000, // 30초 동안 stale data를 보여줌
@@ -14,17 +14,19 @@ const RQSuperHeroes = () => {
             // refetchOnWindowFocus: false, // 브라우저 창이 focus 시 자동으로 쿼리를 실행하는가?
             // refetchInterval: 2000, // 2초마다 자동으로 query를 실행함
             // refetchIntervalInBackground: false // 백그라운드에서도 자동으로 query를 실행함
+            enabled: false
         }
     )
 
     console.log({ isLoading, isFetching })
 
-    if (isLoading) return <h2>Loading...</h2>
+    if (isLoading || isFetching) return <h2>Loading...</h2>
 
     if (isError) return <h2>{error.message}</h2>
     return (
         <div>
             <h2>RQ Super Heroes Page</h2>
+            <button onClick={refetch}>Fetch heroes</button>
             {data?.data.map((hero) => {
                 return <div key={hero.id}>{hero.name}</div>
             })}
