2b6bf61f0fd7d76eb04463978d1063373ce57d8d
unrin <momlin069@gmail.com>
2023-09-25 23:48:33 +0900

    :sparkles: Add axios instance

--- src/hooks/useSuperHeroesData.js
+++ src/hooks/useSuperHeroesData.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
+import { request } from '../utils/axios-utils';
 import { useQuery, useMutation, useQueryClient } from 'react-query';
 
 const fetchSuperHeroes = () => {
-    return axios.get('http://localhost:4000/superheroes')
+    return request({ url: '/superheroes' })
 }
 
 const addSuperHero = (hero) => {
-    return axios.post('http://localhost:4000/superheroes', hero)
+    return request({ url: '/superheroes', method: 'post', data: hero })
 }
 
 const useSuperHeroesData = (onSuccess, onError) => {
